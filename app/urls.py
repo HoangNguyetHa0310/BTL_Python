5d3c2dbc31548fa0973ab9b1e91b355ac04a49ea
HoangNguyetHa0310 <hoangyh2712@gmail.com>
2024-06-04 11:29:59 +0700

    Update : đổi tên file và link dường dẫn các trang sản phẩm trong header

--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,9 @@ urlpatterns = [
     # path('', views.base,name="base"),
     path('', views.index,name="index"),
     path('cart/', views.cart, name="cart"),
-    path('checkout/', views.checkout, name="checkout"),
+    path('product_woman/', views.product_woman, name="product_woman"),
+    path('product_man/', views.product_man, name="product_man"),
+    path('product_kid/', views.product_kid, name="product_kid"),
+    path('new_product/', views.new_product, name="new_product"),
 ]
 
--- app/views.py
+++ app/views.py
@@ -15,3 +15,23 @@ def cart(request):
 def checkout(request):
     context = {}
     return render(request, 'app/checkout.html',context)
+
+
+def product_woman(request):
+    context = {}
+    return render(request, 'app/product_woman.html',context)
+
+
+def product_man(request):
+    context = {}
+    return render(request, 'app/product_man.html',context)
+
+
+def product_kid(request):
+    context = {}
+    return render(request, 'app/product_kid.html',context)
+
+
+def new_product(request):
+    context = {}
+    return render(request, 'app/new_product.html',context)
